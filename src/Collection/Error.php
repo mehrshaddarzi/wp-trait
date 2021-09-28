bc86a2d1babb6defbe3359f5e75ec3712b262e56
Mehrshad Darzi <mehrshad198@gmail.com>
2021-09-28 18:38:32 +0330

    add `WP_Error` Collection

---
+++ src/Collection/Error.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace WPTrait\Collection;
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+if (!class_exists('Error')) {
+
+    class Error
+    {
+        public function has($thing)
+        {
+            return is_wp_error($thing);
+        }
+
+        public function message($thing)
+        {
+            return $thing->get_error_message();
+        }
+
+        public function new($code = '', $message = '', $data = '')
+        {
+            #see: https://code.tutsplus.com/tutorials/wordpress-error-handling-with-wp_error-class-i--cms-21120
+            return new \WP_Error($code = '', $message = '', $data = '');
+        }
+    }
+
+}
