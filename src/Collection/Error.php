8b151cb8075c1a82310d1e411d05a18e5e96b4fd
Mehrshad Darzi <mehrshad198@gmail.com>
2021-11-14 14:16:41 +0330

    add get Error Code Method

--- src/Collection/Error.php
+++ src/Collection/Error.php
@@ -3,28 +3,33 @@
 namespace WPTrait\Collection;
 
 if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly.
+	exit; // Exit if accessed directly.
 }
 
 if (!class_exists('WPTrait\Collection\Error')) {
 
-    class Error
-    {
-        public function has($thing)
-        {
-            return is_wp_error($thing);
-        }
+	class Error
+	{
+		public function has($thing)
+		{
+			return is_wp_error($thing);
+		}
 
-        public function message($thing)
-        {
-            return $thing->get_error_message();
-        }
+		public function message($thing)
+		{
+			return $thing->get_error_message();
+		}
 
-        public function new($code = '', $message = '', $data = '')
-        {
-            #see: https://code.tutsplus.com/tutorials/wordpress-error-handling-with-wp_error-class-i--cms-21120
-            return new \WP_Error($code = '', $message = '', $data = '');
-        }
-    }
+		public function code($thing)
+		{
+			return $thing->get_error_code();
+		}
+
+		public function new($code = '', $message = '', $data = '')
+		{
+			#see: https://code.tutsplus.com/tutorials/wordpress-error-handling-with-wp_error-class-i--cms-21120
+			return new \WP_Error($code = '', $message = '', $data = '');
+		}
+	}
 
 }
