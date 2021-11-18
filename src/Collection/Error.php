86fbe67f12cb8f589ecd2053a595673d1ce11f76
mehrshaddarzi <mehrshad198@gmail.com>
2021-11-18 13:26:38 +0330

    add method for get array of WP_Error data

--- src/Collection/Error.php
+++ src/Collection/Error.php
@@ -3,33 +3,43 @@
 namespace WPTrait\Collection;
 
 if (!defined('ABSPATH')) {
-	exit; // Exit if accessed directly.
+    exit; // Exit if accessed directly.
 }
 
 if (!class_exists('WPTrait\Collection\Error')) {
 
-	class Error
-	{
-		public function has($thing)
-		{
-			return is_wp_error($thing);
-		}
+    class Error
+    {
+        public function has($thing)
+        {
+            return is_wp_error($thing);
+        }
 
-		public function message($thing)
-		{
-			return $thing->get_error_message();
-		}
+        public function message($thing)
+        {
+            return $thing->get_error_message();
+        }
 
-		public function code($thing)
-		{
-			return $thing->get_error_code();
-		}
+        public function messages($thing)
+        {
+            return $thing->get_error_messages();
+        }
 
-		public function new($code = '', $message = '', $data = '')
-		{
-			#see: https://code.tutsplus.com/tutorials/wordpress-error-handling-with-wp_error-class-i--cms-21120
-			return new \WP_Error($code = '', $message = '', $data = '');
-		}
-	}
+        public function code($thing)
+        {
+            return $thing->get_error_code();
+        }
+
+        public function codes($thing)
+        {
+            return $thing->get_error_codes();
+        }
+
+        public function new($code = '', $message = '', $data = '')
+        {
+            #see: https://code.tutsplus.com/tutorials/wordpress-error-handling-with-wp_error-class-i--cms-21120
+            return new \WP_Error($code = '', $message = '', $data = '');
+        }
+    }
 
 }
