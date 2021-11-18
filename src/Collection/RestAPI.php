63eb7d746a4d364397ab839a316dbdf89604869a
mehrshaddarzi <mehrshad198@gmail.com>
2021-11-18 13:27:35 +0330

    add internal request REST API

--- src/Collection/RestAPI.php
+++ src/Collection/RestAPI.php
@@ -11,13 +11,54 @@ if (!class_exists('WPTrait\Collection\RestAPI')) {
     class RestAPI
     {
 
-        public function request($type = 'GET', $route = '', $params = [])
+        public function internal($type = 'GET', $route = '', $arg = [])
         {
-            $request = new \WP_REST_Request($type, '/' . $route);
-            $request->set_query_params($params);
-            $response = rest_do_request($request);
-            $server = rest_get_server();
-            return $server->response_to_data($response, false);
+            $defaults = [
+                'body' => [],
+                'query' => [],
+                'headers' => [],
+                'file' => []
+            ];
+            $args = wp_parse_args($arg, $defaults);
+
+            $request = new \WP_REST_Request($type, '/' . ltrim($route, "/"));
+
+            # Header
+            if (!empty($args['headers'])) {
+                $request->set_headers($args['headers'], $override = true);
+            }
+
+            # query [GET]
+            if (!empty($args['query'])) {
+                $request->get_query_params($args['query']);
+            }
+
+            # body [POST]
+            if (!empty($args['body'])) {
+                $request->set_body_params($args['body']);
+            }
+
+            # file [FILE]
+            if (!empty($args['file'])) {
+                $request->set_file_params($args['file']);
+            }
+
+            $do = rest_do_request($request);
+            if ($do->is_error()) {
+                $error_data = $do->get_error_data();
+                $status = isset($error_data['status']) ? $error_data['status'] : 500;
+                return [
+                    'error' => true,
+                    'message' => $do->get_error_message(),
+                    'response' => array(
+                        'code' => $status,
+                        'message' => get_status_header_desc($status),
+                    )
+                ];
+            }
+
+            # wp-includes/class-wp-http-requests-response.php#L185
+            return $do->to_array();
         }
 
         public function url($path = '/', $blog_id = null, $scheme = 'rest')
