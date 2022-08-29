bab3d0d1a5d4b878d8fd21045c7cbb6895a33a7a
Mehrshad Darzi <mehrshad198@gmail.com>
2022-08-30 00:08:27 +0430

    add Submenus when define admin Menu

--- src/Hook/AdminMenu.php
+++ src/Hook/AdminMenu.php
@@ -27,9 +27,14 @@ if (!trait_exists('WPTrait\Hook\AdminMenu')) {
             global $menu, $submenu;
         }
 
-        public function add_menu($page_title = '', $menu_title = '', $capability = 'manage_options', $menu_slug = '', $function = '', $icon_url = '', $position = null)
+        public function add_menu($page_title = '', $menu_title = '', $capability = 'manage_options', $menu_slug = '', $function = '', $icon_url = '', $position = null, $submenus = [])
         {
             add_menu_page($page_title, ($menu_title == "" ? $page_title : $menu_title), $capability, $menu_slug, $function, $icon_url, $position);
+            if (!empty($submenus) and is_array($submenus) and count($submenus) > 0) {
+                foreach ($submenus as $submenu) {
+                    $this->add_submenu(($submenu['parent_slug'] ?? $menu_slug), $submenu['page_title'], ($submenu['menu_title'] == "" ? $submenu['page_title'] : $submenu['menu_title']), $submenu['capability'], $submenu['menu_slug'], $submenu['function'], ($submenu['position'] ?? null));
+                }
+            }
         }
 
         public function add_submenu($parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function = '', $position = null)
