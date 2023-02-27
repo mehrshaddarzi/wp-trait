edeb842f8b1c2088809ffd1a04a14a382bd37710
1mahdimf <1mahdimf@users.noreply.github.com>
2023-02-28 01:34:20 +0330

    Update Route.php
    افزودن امکان استفاده از قالب و json و .... 
    در route

--- src/Hook/Route.php
+++ src/Hook/Route.php
@@ -9,53 +9,100 @@ if (!defined('ABSPATH')) {
 
 if (!trait_exists('WPTrait\Hook\Route')) {
 
-
 trait Route
 {
 
 
+
+
     public function bootRoute($arg = [])
     {
-        $this->addRoute();
-        
-        if (  defined( 'WP_DEBUG' ) ) {
+        if (empty($arg)) return;
+
+        $this->addRoute($arg);
+
+        if (WP_DEBUG) {
             flush_rewrite_rules();
         }
-        
     }
 
-     public function addRoute()
+    public function addRoute($arg)
     {
-        foreach ($this->Route as $routeName => $RouteArg) {
+        foreach ($arg as $routeName => $RouteArg) {
             $this->addRewriteRule($routeName, $RouteArg['tags']);
-            if (!empty($RouteArg['template']) && method_exists($this, $RouteArg['template'])) {
-                add_filter('template_include', [$this, $RouteArg['template']]);
-            }
-            if (!empty($RouteArg['title']) && method_exists($this, $RouteArg['title'])) {
-                add_filter('pre_get_document_title', [$this, $RouteArg['title']]);
+            if (!empty($RouteArg['cb']) && method_exists($this, $RouteArg['cb'])) {
+
+                if (!empty($RouteArg['template'])) {
+                    add_filter('template_include', function ($template) use ($RouteArg) {
+                        return  $this->{$RouteArg['cb']}($template);
+                        exit;
+                    });
+                } else {
+                    add_filter('template_redirect', function () use ($RouteArg) {
+                        $this->ajaxHeader();
+                        $this->{$RouteArg['cb']}();
+                        exit;
+                    });
+                }
+                if (!empty($RouteArg['title']) && method_exists($this, $RouteArg['title'])) {
+                    add_filter('pre_get_document_title', [$this, $RouteArg['title']]);
+                }
             }
         }
-        
     }
-  public function addRewriteRule($routeName, $queryNames)
+
+    public function addRewriteRule($routeName, $queryNames)
     {
         $query = '';
         $key = 1;
         foreach ($queryNames  as $keyName => $value) {
-            if(is_numeric($keyName)){
+            if (is_numeric($keyName)) {
                 $query .= $value . '=$matches[' . $key . ']&';
                 $key++;
                 add_rewrite_tag('%' . $value . '%', '([^&/]+)');
-            }else{
-                $query .= $keyName . '='.$value.'&';
+            } else {
+                $query .= $keyName . '=' . $value . '&';
                 add_rewrite_tag('%' . $keyName . '%', '([^&/]+)');
             }
-
-            
         }
 
-        add_rewrite_rule($routeName, 'index.php?' . $query, 'top');
 
+        add_rewrite_rule($routeName, 'index.php?' . rtrim($query, '&'), 'top');
+
+        //add_filter('generate_rewrite_rules', function($i){db($i->rules);}, 10);
+
+
+    }
+
+
+    public function ajaxHeader()
+    {
+
+
+        if (!WP_DEBUG || (WP_DEBUG && !WP_DEBUG_DISPLAY)) {
+            @ini_set('display_errors', 0); // Turn off display_errors during AJAX events to prevent malformed JSON.
+        }
+
+
+        if (!defined("DOING_AJAX")) define("DOING_AJAX", true);
+        if (!defined("DONOTCACHEPAGE")) define("DONOTCACHEPAGE", true);
+        if (!defined("DONOTCACHEOBJECT")) define("DONOTCACHEOBJECT", true);
+        if (!defined("DONOTCACHEDB")) define("DONOTCACHEDB", true);
+
+
+        $this->db->hide_errors();
+
+        if (!headers_sent()) {
+            send_origin_headers();
+            send_nosniff_header();
+            nocache_headers();
+            header('Content-Type: text/html; charset=' . get_option('blog_charset'));
+            header('X-Robots-Tag: noindex');
+            status_header(200);
+        } elseif (WP_DEBUG) {
+            headers_sent($file, $line);
+            trigger_error("ajax_headers cannot set headers - headers already sent by {$file} on line {$line}", E_USER_NOTICE);
+        }
     }
 }
 
