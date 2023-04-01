8aede0c019131da4f45ca81c6b89e773bf277014
1mahdimf <1mahdimf@users.noreply.github.com>
2023-04-01 14:38:27 +0330

    Update Singleton.php
    خوانایی بهتر و رفع خطا

--- src/Utils/Singleton.php
+++ src/Utils/Singleton.php
@@ -4,13 +4,13 @@ namespace WPTrait\Utils;
 
 trait Singleton
 {
-    /** 
-    * Collection of instance.
-    * @var self|array 
-    */
-    private static $instance = [];
+	/** 
+	 * Collection of instance.
+	 * @var self|array 
+	 */
+	private static $instance = [];
 
-    
+	/** 
 	 * This method returns new or existing Singleton instance
 	 * of the class for which it is called. This method is set
 	 * as final intentionally, it is not meant to be overridden.
@@ -18,7 +18,7 @@ trait Singleton
 	 * @return object Singleton instance of the class.
 	 */
 	final public static function instance(): self
-     {
+	{
 		/**
 		 * If this trait is implemented in a class which has multiple
 		 * sub-classes then static::$_instance will be overwritten with the most recent
@@ -29,27 +29,26 @@ trait Singleton
 		 */
 		$called_class = get_called_class();
 
-		if ( ! isset( $instance[ $called_class ] ) ) {
+		if (!isset($instance[$called_class])) {
 
-			$instance[ $called_class ] = new $called_class();
+			$instance[$called_class] = new $called_class();
 
 			/**
 			 * Dependent items can use the `singleton_init_{$called_class}` hook to execute code
 			 */
-			do_action( sprintf( 'singleton_init_%s', $called_class ) ); // phpcs:ignore WordPress.NamingConventions.ValidHookName.UseUnderscores, WordPress.NamingConventions.PrefixAllGlobals.DynamicHooknameFound
+			do_action(sprintf('singleton_init_%s', $called_class)); // phpcs:ignore WordPress.NamingConventions.ValidHookName.UseUnderscores, WordPress.NamingConventions.PrefixAllGlobals.DynamicHooknameFound
 
 		}
 
-		return $instance[ $called_class ];
-
+		return $instance[$called_class];
 	}
-    private function __construct()
-    {
-        // Intentionally empty
-    }
+	private function __construct()
+	{
+		// Intentionally empty
+	}
 
-    private function __clone()
-    {
-        // Intentionally empty
-    }
+	private function __clone()
+	{
+		// Intentionally empty
+	}
 }
