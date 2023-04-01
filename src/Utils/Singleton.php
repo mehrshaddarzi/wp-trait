1cbcdc382bcacfa97bdbfc0f0514cd2ca4a0d4dc
1mahdimf <1mahdimf@users.noreply.github.com>
2023-04-01 14:27:55 +0330

    Update Singleton.php
    تغییراتی برای هماهنگی با
    sub-classes
    همچنین افزودن
    do_action

--- src/Utils/Singleton.php
+++ src/Utils/Singleton.php
@@ -4,22 +4,45 @@ namespace WPTrait\Utils;
 
 trait Singleton
 {
-    /** @var self|null */
-    private static $instance = null;
+    /** 
+    * Collection of instance.
+    * @var self|array 
+    */
+    private static $instance = [];
 
-    /**
-     * @return static
-     * @psalm-return self
-     */
-    final public static function instance(): self
-    {
-        if (self::$instance === null) {
-            self::$instance = new self();
-        }
+    
+	 * This method returns new or existing Singleton instance
+	 * of the class for which it is called. This method is set
+	 * as final intentionally, it is not meant to be overridden.
+	 *
+	 * @return object Singleton instance of the class.
+	 */
+	final public static function instance(): self
+     {
+		/**
+		 * If this trait is implemented in a class which has multiple
+		 * sub-classes then static::$_instance will be overwritten with the most recent
+		 * sub-class instance. Thanks to late static binding
+		 * we use get_called_class() to grab the called class name, and store
+		 * a key=>value pair for each `classname => instance` in self::$_instance
+		 * for each sub-class.
+		 */
+		$called_class = get_called_class();
 
-        return self::$instance;
-    }
+		if ( ! isset( $instance[ $called_class ] ) ) {
 
+			$instance[ $called_class ] = new $called_class();
+
+			/**
+			 * Dependent items can use the `singleton_init_{$called_class}` hook to execute code
+			 */
+			do_action( sprintf( 'singleton_init_%s', $called_class ) ); // phpcs:ignore WordPress.NamingConventions.ValidHookName.UseUnderscores, WordPress.NamingConventions.PrefixAllGlobals.DynamicHooknameFound
+
+		}
+
+		return $instance[ $called_class ];
+
+	}
     private function __construct()
     {
         // Intentionally empty
